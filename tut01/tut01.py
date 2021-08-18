bf9201730c1d21cee987067f59e0b40ed566a16f
Narendra Tejasvi <narendratheultimate@gmail.com>
2021-08-18 16:39:00 +0530

    changed

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -14,16 +14,16 @@ def meraki_helper(n):
         mod1=mod2 
     return True         
  
- //solved
+
 input = [12, 14, 56, 78, 98, 54, 678, 134, 789, 0, 7, 5, 123, 45, 76345, 987654321] 
-count1=count2=0 
+meraki=nonmeraki=0 
  
 for index in range(len(input)): 
     if meraki_helper(input[index])==True: 
-        count1+=1 
+        meraki+=1 
         print("Yes - {0} is a Meraki number".format(input[index])) 
     else: 
-        count2+=1 
+        nonmeraki+=1 
         print("No - {0} is not a Meraki number".format(input[index]))     
  
-print("the input list contains {0} meraki and {1} non meraki numbers".format(count1,count2))
+print("the input list contains {0} meraki and {1} non meraki numbers".format(meraki,nonmeraki))
